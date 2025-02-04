a77309a23e22e7a8ea4275b77bdc163871b2541f
cinemahdmovie <ak2248274@gmail.com>
2025-02-04 23:11:23 +0530

    Update info.py

--- info.py
+++ info.py
@@ -45,7 +45,7 @@ reqst_channel = environ.get('REQST_CHANNEL', '-1002314516275')
 REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_channel) else None
 
 # This Is Your Bot Support Group Id , Here Bot Will Not Give File Because This Is Support Group.
-support_chat_id = environ.get('SUPPORT_CHAT_ID', '-1002314516275')
+support_chat_id = environ.get('SUPPORT_CHAT_ID', '-1002424461238')
 SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(support_chat_id) else None
 
 # This Channel Is For Index Request 
